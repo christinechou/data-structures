51fd99dad833d131784fed54082e20f9783815f9
christinechou <christinejchou@gmail.com>
2016-06-08 15:01:29 -0700

    Implemented push method

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,19 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var key = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[key] = value;
+    key += 1;
   };
 
   someInstance.pop = function() {
   };
 
   someInstance.size = function() {
+    return key;
   };
 
   return someInstance;
