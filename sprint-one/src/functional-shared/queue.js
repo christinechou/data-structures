46bd7780975807632070cb10a133b5309887d862
christinechou <christinejchou@gmail.com>
2016-06-08 16:20:58 -0700

    Implement queue function and bind the methods object Implement size and queue methods

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,23 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var queue = {};
+  queue.storage = {}; 
+  queue.first = 0;
+  queue.last = 0;
+
+  return _.extend(queue, Queue.queueMethods);
+
 };
 
-var queueMethods = {};
 
 
+Queue.queueMethods = {
+  size: function() {
+    return this.last - this.first;
+  },
+  enqueue : function(value) {
+    this.storage[this.last];
+    this.last += 1;
+  }
+};
