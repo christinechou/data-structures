47744a68ce5456fb22cdbd318db91fdce0d10c33
christinechou <christinejchou@gmail.com>
2016-06-08 21:53:13 -0700

    Implement enqueue prototype method

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -8,6 +8,10 @@ Queue.prototype.size = function() {
   return this.last - this.first;
 };
 
+Queue.prototype.enqueue = function(value) {
+  this.storage[this.last] = value;
+  this.last += 1;
+};
 
 
 
