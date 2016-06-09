5b7a7fb5b7680bf3a49d0c030b62efeac1fa8f5c
christinechou <christinejchou@gmail.com>
2016-06-08 21:12:18 -0700

    Add storage property Implement push method

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,5 +1,6 @@
 var Stack = function() {
   var stack = Object.create(Stack.stackMethods);
+  stack.storage = {};
   stack.key = 0;
 
   return stack;
@@ -8,6 +9,19 @@ var Stack = function() {
 var stackMethods = {
   size : function() {
     return this.key;
+  }, 
+  pop : function() {
+    var temp = this.storage[this.key - 1];
+    if(this.key > 0){
+      delete  this.storage[this.key];
+      this.key -= 1;
+    }
+    
+    return temp;
+  }, 
+  push : function(value) {
+    this.storage[this.key] = value;
+    this.key += 1;
   }
 };
 
