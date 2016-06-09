8a7786d2a450866f8a8f2ba54eff41483dd8ec0b
christinechou <christinejchou@gmail.com>
2016-06-09 09:57:37 -0700

    Implement addToTail method on the link object

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,22 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    //link new node to end of linked list
+    var node = Node(value);
+    if ( !this.head ) {
+      this.head = this.tail = node;
+    } else {
+      this.tail.next = node;
+      this.tail = node;
+    }
   };
 
   list.removeHead = function() {
+    //remove node at head and set previous head.next as head
   };
 
   list.contains = function(target) {
+    //loop through the linked list to find the target value
   };
 
   return list;
@@ -23,6 +33,7 @@ var Node = function(value) {
 
   return node;
 };
+
 
 /*
  * Complexity: What is the time complexity of the above functions?
