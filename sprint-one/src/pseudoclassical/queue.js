14307db75fe99959fcf6c6cf410c99136080d6d8
christinechou <christinejchou@gmail.com>
2016-06-08 21:56:20 -0700

    Implement dequeue prototype method

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -13,5 +13,18 @@ Queue.prototype.enqueue = function(value) {
   this.last += 1;
 };
 
+Queue.prototype.dequeue = function() {
+  var temp = this.storage[this.first];
+
+  if(this.last > this.first) {
+    delete this.storage[this.first];
+    this.first += 1;
+  }
+
+  return temp;
+};
+
+
+
 
 
