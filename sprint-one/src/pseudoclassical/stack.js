d566c2df2385a8ebd780a4b542b7b50134cf3650
christinechou <christinejchou@gmail.com>
2016-06-08 21:30:56 -0700

    Implement pseudoclassical constructor and size method

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,14 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.key = 0;
 };
 
 
+Stack.prototype = {
+  size: function() {
+    return this.key;
+  }
+}
+
+// var s = new Stack();
+// s.push('apple');
