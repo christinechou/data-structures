9dab4e4d40afb8362e33a9eb98414e5c999258bc
christinechou <christinejchou@gmail.com>
2016-06-08 21:21:00 -0700

    Implement constructor function Implement the enqueue and size method prototypes

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,21 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+
+  var queue = Object.create(queueMethods);
+  queue.storage = {};
+  queue.first = 0;
+  queue.last = 0;
+
+  return queue;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  size: function() {
+    return this.last - this.first;
+  },
+  enqueue: function(value) {
+    this.storage[this.last] = value;
+    this.last += 1;
+  }
+};
 
 
