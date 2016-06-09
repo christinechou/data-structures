6285129111c8eedf5d0cd7a1e32e04001599637f
christinechou <christinejchou@gmail.com>
2016-06-08 20:00:42 -0700

    Implement function constructor

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,6 +1,10 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var stack = {};
+  stack.storage = {};
+  stack.key = 0;
+  return _.extend(stack, stackMethods);
 };
 
 var stackMethods = {};
