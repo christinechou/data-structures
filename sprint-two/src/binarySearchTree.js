487ab0abb7e79d2b16f3c8d7e73020838f081372
christinechou <christinejchou@gmail.com>
2016-06-10 16:31:54 -0700

    Implement insert method

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,37 @@
 var BinarySearchTree = function(value) {
+  var tree = Object.create(BinaryTreeMethds);
+  tree.value = value;
+  tree.left = null;
+  tree.right = null;
+
+  return tree;
 };
 
+var BinaryTreeMethds = {
+  insert: function(value) { 
+    var pointer = this;
+    
+    while ( pointer.value !== value) {
+      if (pointer.value > value) {
+        if (!pointer.left) {
+          pointer.left = BinarySearchTree(value);
+        }
+        pointer = pointer.left;
+      } else {
+        if (!pointer.right) {
+          pointer.right = BinarySearchTree(value);
+        }
+        pointer = pointer.right;
+      }
+    }
+  },
+  contains: function() {
+
+  },
+  depthFirstLog: function() {
+
+  }
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
