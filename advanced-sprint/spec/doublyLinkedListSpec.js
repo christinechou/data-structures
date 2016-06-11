d6192f1919afc069b5db5678b98725a5270150b0
christinechou <christinejchou@gmail.com>
2016-06-11 10:37:41 -0700

    Implement test specificatons for doublyLinks

--- advanced-sprint/spec/doublyLinkedListSpec.js
+++ advanced-sprint/spec/doublyLinkedListSpec.js
@@ -22,13 +22,16 @@ describe('doublyLinkedList', function() {
     expect(doublyLinkedList.addToTail).to.be.a('function');
     expect(doublyLinkedList.removeHead).to.be.a('function');
     expect(doublyLinkedList.contains).to.be.a('function');
+    expect(doublyLinkedList.addToHead).to.be.a('function');
+    expect(doublyLinkedList.removeTail).to.be.a('function');
   });
 
   it('should designate a new tail when new nodes are added', function() {
     doublyLinkedList.addToTail(4);
-    expect(doublyLinkedList.tail.value).to.equal(4);
-    doublyLinkedList.addToTail(5);
-    expect(doublyLinkedList.tail.value).to.equal(5);
+    doublyLinkedList.addToTail(7);
+    expect(doublyLinkedList.tail.value).to.equal(7);
+    expect(doublyLinkedList.tail.previous.value).to.equal(4);
+    expect(doublyLinkedList.tail.previous.next.value).to.equal(7);
   });
 
   it('should remove the head from the list when removeHead is called', function() {
@@ -38,6 +41,27 @@ describe('doublyLinkedList', function() {
     doublyLinkedList.removeHead();
     expect(doublyLinkedList.head.value).to.equal(5);
   });
+
+  it('should remove the tail from the list when removeTail is called', function() {
+    doublyLinkedList.addToTail(4);
+    doublyLinkedList.addToTail(5);
+    var previous = doublyLinkedList.tail.previous;
+    doublyLinkedList.removeTail();
+    expect(doublyLinkedList.tail.value).to.equal(4);
+    expect(previous.next).to.equal(null);
+  });
+
+  it('should add head to the list when addToHead is called', function() {
+    doublyLinkedList.addToTail(5);
+    var previousHead = doublyLinkedList.head;
+    doublyLinkedList.addToTail(3);
+    doublyLinkedList.addToTail(7);
+    doublyLinkedList.addToHead(4);
+    expect(doublyLinkedList.head.value).to.equal(4);
+    expect(previousHead.previous).to.equal(doublyLinkedList.head);
+  });
+
+
 
   it('should return the value of the former head when removeHead is called', function() {
     doublyLinkedList.addToTail(4);
