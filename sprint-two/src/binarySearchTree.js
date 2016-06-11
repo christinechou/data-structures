f423b6125c643b9cc4569b8906cf98357c013876
christinechou <christinejchou@gmail.com>
2016-06-10 21:43:30 -0700

    Implement BinarySearchTree construct Implement insert method Implement constains method Implement depthFirstLog method

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -29,32 +29,38 @@ var BinaryTreeMethds = {
     var pointer = this;
 
     while (pointer) {
-      if(pointer.value === value) {
+      if (pointer.value === value) {
         return true;
       }
-      if(!(pointer.left || pointer.right) && pointer.value !== value){
+      if (!(pointer.left || pointer.right)) {
         return false;
       }
 
-      if (pointer.value < value) {
-        if (!pointer.left) {
-          pointer.left = BinarySearchTree(value);
-        }
+      if (pointer.value > value) {
         pointer = pointer.left;
       } else {
-        if (!pointer.right) {
-          pointer.right = BinarySearchTree(value);
-        }
         pointer = pointer.right;
       }
     }
 
   },
-  depthFirstLog: function() {
+  depthFirstLog: function(fn) {
+    var pointer = this;
 
+    fn(pointer.value);
+    if (pointer.left !== null) {
+      pointer.left.depthFirstLog(fn);
+    }
+
+    if (pointer.right !== null) {
+      pointer.right.depthFirstLog(fn);
+    }
   }
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ .contains :  O(log(n))
+ .insert: O(log(n))
+ .depthFirstLog: O(n)
  */
