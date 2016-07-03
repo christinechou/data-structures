409d9293a85b5fa0644c627541e12d863594f68c
Christine Chou <christinejchou@gmail.com>
2016-07-03 14:01:14 -0700

    Refactor hash table implementation

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,39 +1,107 @@
 
 
 var HashTable = function() {
+
+  this._size = 0;
+    this._limit = 8;
+  this._storage = LimitedArray(this._limit);
+};
+
+
+/////////////////////////////
+// Implemented using HOF
+/////////////////////////////
+
+var HashTableHOF = function() {
+  this._size = 0;
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
 };
 
-HashTable.prototype.insert = function(k, v) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
-  this._storage.set(index, v);
-};
-
-HashTable.prototype.retrieve = function(k) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
-  return this._storage.get(index);
-};
-
-HashTable.prototype.remove = function(k) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
-  var h;
-  this._storage.each(function(val, key) {
-    if (index === key) {
-      h = new HashTable();
-    }
-  });
-
-  if (h !== undefined) {
-    this._storage.each(function(val, key) {
-      if (index !== key) {
-        h.insert(key, val);
+HashTableHOF.prototype.insert = function(k, v) {
+  return this._tupleSearch(k,
+    function(bucket, tuple, i) {
+      var oldValue = tuple[1];
+      tuple[1] = v;
+      return oldValue;
+    },
+    function(bucket) {
+      bucket.push([k, v]);
+      this._size++;
+      if (this._size > 0.75 * this._limit) {
+        this._resize( this._limit * 2 );
       }
-    });
-
-  }
-  this._storage = h._storage;
+    }
+  );
 };
+
+HashTableHOF.prototype.retrieve = function(k) {
+  return this._tupleSearch(k,
+    function(bucket, tuple, i) {
+      return tuple[1];
+    }
+  );
+};
+
+HashTableHOF.prototype.remove = function(k) {
+  return this._tupleSearch(k,
+    function(bucket, tuple, i) {
+      bucket.splice(i, 1);
+      this._size--;
+      if (this._size < 0.25 * this._limit) {
+        this._resize(Math.floor(this._limit / 2));
+      }
+      return tuple[1];
+    }
+  );
+};
+
+HashTableHOF.prototype._tupleSearch = function(key, foundCB, notFoundCB) {
+  var index = getIndexBelowMaxForKey(key, this._limit);
+  var bucket = this._storage.get(index) || [];
+
+  this._storage.set(index, bucket);
+
+  for (var i = 0; i < bucket.length; i++) {
+    var tuple = bucket[i];
+    if (tuple[0] === key) {
+      return foundCB.call(this, bucket, tuple, i);
+    }
+  }
+
+  return notFoundCB ? notFoundCB.call(this, bucket) : undefined;
+};
+
+HashTableHOF.prototype._resize = function(newLimit) {
+  var oldStorage = this._storage;
+
+  // min size of 8, return if nothing to do!
+  newLimit = Math.max(newLimit, 8);
+  if (newLimit === this._limit) { return; }
+
+  this._limit = newLimit;
+  this._storage = LimitedArray(this._limit);
+  this._size = 0;
+
+  oldStorage.each(this._redistribute.bind(this));
+};
+
+HashTableHOF.prototype._redistribute = function(bucket) {
+  if (!bucket) { return; }
+  for (var i = 0; i < bucket.length; i++) {
+    var tuple = bucket[i];
+    this.insert(tuple[0], tuple[1]);
+  }
+};
+
+HashTable = HashTableHOF;
+
+
+// var h = new HashTable()
+// h.insert('a','apple');
+// h.insert('b', 'banana')
+// h.insert('c', 'cat');
+// h.remove('c')
 
 
 
